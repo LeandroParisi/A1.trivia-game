28b883c0b36224e5cae9787ef59bf2b89580c073
LeandroParisi <lepcbelisario@gmail.com>
2020-11-11 09:58:02 -0300

    updating GH Pages

---
+++ build/precache-manifest.708bb34c5f5bcf161bbd0be157782ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310a56a0f9929a638171f7cc8e849654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf8e39d483a6586ca685",
+    "url": "/static/css/main.8ac52c6b.chunk.css"
+  },
+  {
+    "revision": "057b4c5ddd76debdbe59",
+    "url": "/static/js/2.859be2f7.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.859be2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8e39d483a6586ca685",
+    "url": "/static/js/main.8dcf33ed.chunk.js"
+  },
+  {
+    "revision": "f8a4e7c1de7ec49c0042",
+    "url": "/static/js/runtime-main.e2209b7b.js"
+  }
+]);
