55d6e3103284d75cf5541a410ebc90155a5f95bb
LeandroParisi <lepcbelisario@gmail.com>
2020-11-15 16:22:04 -0300

    Trying GH pages again

---
+++ build/precache-manifest.f74953bd4619e200ea1e116380ee5e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d9d5c151b3f82d17147166b11da05f",
+    "url": "/A1.trivia-game/index.html"
+  },
+  {
+    "revision": "5b7c3e5642e04772e823",
+    "url": "/A1.trivia-game/static/css/main.e853ced5.chunk.css"
+  },
+  {
+    "revision": "f1ee2ec54a67597fc013",
+    "url": "/A1.trivia-game/static/js/2.fe725652.chunk.js"
+  },
+  {
+    "revision": "39803453d936ee949594b38b562f63de",
+    "url": "/A1.trivia-game/static/js/2.fe725652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7c3e5642e04772e823",
+    "url": "/A1.trivia-game/static/js/main.5734d1ba.chunk.js"
+  },
+  {
+    "revision": "5e47e057ce3b41f6fa91",
+    "url": "/A1.trivia-game/static/js/runtime-main.4d7a9085.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.708bb34c5f5bcf161bbd0be157782ac0.js"
+  "/A1.trivia-game/precache-manifest.f74953bd4619e200ea1e116380ee5e53.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/A1.trivia-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
